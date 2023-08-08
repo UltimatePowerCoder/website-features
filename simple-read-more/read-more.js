0a9254d09c38181475588357e9cb75212818c1eb
Leonid Korshunov <ultimatepowercoder@gmail.com>
2023-08-08 13:19:43 +0400

    Добавил еще один вариант.

--- simple-read-more/read-more.js
+++ simple-read-more/read-more.js
@@ -3,3 +3,21 @@ $('.read-less, .hide').hide();
 $('.read-more, .read-less').click(function() {
   $('.read-more, .read-less, .hide').toggle();
 });
+
+// Пример - 2
+
+function readMore(){
+  let dots = document.getElementById("dots");
+  let more = document.getElementById("more");
+  let btn = document.getElementById("btn");
+
+  if(dots.style.display === "none") {
+    dots.style.display = "inline";
+    btn.innerHTML = "Read More";
+    more.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btn.innerHTML = "Hide";
+    more.style.display = "inline";
+  }
+}
